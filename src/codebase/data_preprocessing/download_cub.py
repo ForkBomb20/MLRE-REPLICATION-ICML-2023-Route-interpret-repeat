076aa65b214df8c37576a6d71faa05e5681bfc3e
minjshin <minjshin@umich.edu>
2025-11-02 18:53:20 -0500

    dataset creation validation

--- src/codebase/data_preprocessing/download_cub.py
+++ src/codebase/data_preprocessing/download_cub.py
@@ -10,21 +10,8 @@ def download_cub(root="CUB_200_2011", force=True):
 
     os.system("mv ../attributes.txt .")
     os.rename("attributes.txt", "attributes_names.txt")
-    # attribute_groups = {}
-    # with open("attributes_names.txt", "r") as f:
-    #     lines = f.readlines()
-    #     for line in lines:
-    #         fields = line.split(" ")
-    #         attr_idx = int(fields[0]) - 1
-    #         cur_prefix = fields[1].split(":")[0]
-    #         if cur_prefix in attribute_groups:
-    #             attribute_groups[cur_prefix].append(attr_idx)
-    #         else:
-    #             attribute_groups.update({cur_prefix: [attr_idx]})
-    # with open("attribute_groups.json", "w") as f:
-    #     json.dump(attribute_groups, f)
-    # print("Attribute groups saved")
-
+    
+    # 312 attributes (rows), each row is attr_id - attr name
     with open("attributes_names.txt", "r") as f:
         attribute_names = []
         lines = f.readlines()
@@ -32,16 +19,13 @@ def download_cub(root="CUB_200_2011", force=True):
             attr_name = line.split(" ")[1]
             attribute_names.append(attr_name)
 
-    attribute_per_class = pd.read_csv(os.path.join("attributes", "class_attribute_labels_continuous.txt"), sep=" ",
-                                      header=None).to_numpy()
-    print("Attribute per class loaded")
-
+    # ============================================================================================
+    ## Originial attributes ##
+    # 11788 total images (rows), each row is image_id - class_id
     classes = pd.read_csv("image_class_labels.txt", sep=" ", header=None).to_numpy()[:, 1]
     print("Image_classes loaded")
 
-    tot_annotations, count = 0, 0
     attributes = np.zeros(shape=(11788, 312))
-    # denoised_attributes = np.zeros(shape=(11788, 312))
     with open(os.path.join("attributes", "image_attribute_labels.txt"), "r") as f:
         lines = f.readlines()
         for line in lines:
@@ -49,50 +33,44 @@ def download_cub(root="CUB_200_2011", force=True):
             if fields[2] == "1":
                 img_idx = int(fields[0]) - 1
                 attr_idx = int(fields[1]) - 1
-                # key = [key for (key, values) in attribute_groups.items() if attr_idx in values][0]
-                # values = attribute_groups[key]
-                # cls = classes[img_idx] - 1
-                # group_attribute_values = attribute_per_class[cls, values]
-                # correct_attribute = np.argmax(group_attribute_values) + values[0]
                 attributes[img_idx][attr_idx] = 1
-                # denoised_attributes[img_idx][correct_attribute] = 1
-                # if correct_attribute != attr_idx:
-                #     count += 1
-                # tot_annotations += 1
 
+    # total images (11788) * total attributes (312), each cell is 1 if present, 0 if not based on human label
     np.save("original_attributes.npy", attributes)
-    print("Original attributes saved")
-    # np.save("denoised_attributes.npy", denoised_attributes)
-    # print(f"{count} attribute over {tot_annotations} annotations corrected")
-    # print("Denoised attributes Saved")
+
+    # ============================================================================================
+    ## Denoised attributes ##
+    # 200 lines (one class, same order as classes.txt), 312 space-separated cols (% of attribute in class, same order as attributes.txt)
+    attribute_per_class = pd.read_csv(os.path.join("attributes", "class_attribute_labels_continuous.txt"), sep=" ",
+                                      header=None).to_numpy()
 
     very_denoised_attributes = np.zeros(shape=(11788, 312))
-    attribute_sparsity = np.zeros(attributes.shape[1])
+    attribute_sparsity = np.zeros(attributes.shape[1])  # zeros of length 312
     for c in np.unique(classes):
         imgs = classes == c
-        # class_attributes = np.mean(attributes[imgs], axis=0)
         class_attributes = attribute_per_class[c - 1, :] > 50
         very_denoised_attributes[imgs, :] = class_attributes
         attribute_sparsity += class_attributes
     attributes_to_filter = attribute_sparsity < 10
-    # very_denoised_attributes[:, attributes_to_filter] = 0
     very_denoised_attributes = very_denoised_attributes[:, ~attributes_to_filter]
-    # attributes_filtered = np.sum(1 - attributes_to_filter)
-    attributes_filtered = np.sum(attributes_to_filter)
-    print("Number of attributes remained", attributes_filtered)
-    np.save("attributes.npy", very_denoised_attributes)
-    print("Denoised attributes Saved")
 
+    # total images (11788) * total attributes (312), each cell is 1 if over 50% of that class has the attr, 0 if less; based on does the class have that attribute (class_attribute_labels)
+    # Only the attributes that are present in >=10 classes are chosen
+    # Same class has same list of attributes
+    np.save("attributes.npy", very_denoised_attributes)
+
+    # Filter attributes based on above criteria
     with open("attributes_names.txt", "w") as f:
         json.dump(np.asarray(attribute_names)[~attributes_to_filter].tolist(), f)
-    print("Attribute names saved")
 
+    # ============================================================================================
+    # Clean up
     for item in os.listdir():
-        if item not in ["images", "images.txt", "attributes_names.txt", "attribute_groups.json",
+        if item not in ["images", "images.txt", "attributes_names.txt", "README",
                         "original_attributes.npy",  "attributes.npy"]:
             os.system(f"rm -r {item}")
-    print("Temporary file cleaned")
 
+    # Rename images
     os.system("mv images/* .")
     with open("images.txt", "r") as f:
         lines = f.readlines()
@@ -106,10 +84,9 @@ def download_cub(root="CUB_200_2011", force=True):
     os.system("rm -r images")
     os.remove("images.txt")
     os.system("mv ../CUB_200_2011 ../data")
-    print("Images sorted and renamed")
-    print("Dataset configured correctly")
 
     os.chdir(origin_dir)
+    print("Dataset configured correctly")
 
 
 if __name__ == "__main__":
