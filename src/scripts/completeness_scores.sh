9f5a24e64f3a1aea5792fbb61b6893414b36e17c
Nathan James Kawamoto <njkamoto@gl-login1.arc-ts.umich.edu>
2025-11-14 15:59:49 -0500

    Add t only runs for Cyrus

--- src/scripts/completeness_scores.sh
+++ src/scripts/completeness_scores.sh
@@ -8,11 +8,13 @@ slurm_output_cub_resnet_concept_mask=cub_resnet_concept_mask_$CURRENT.out
 slurm_output_cub_resnet_concept_completeness=cub_resnet_concept_completeness_$CURRENT.out
 
 echo "Completeness Scores"
-source path-of-conda/anaconda3/etc/profile.d/conda.sh
-conda activate python_3_7_rtx_6000
+module load python/3.13.2
+echo "CUB-200 ResNet101"
+source ./env/bin/activate
+which python
 
 # MoIE
 
-python ../codebase/concept_completeness_concept_mask_main.py --model "MoIE" --arch "ResNet101" --dataset "cub" --iterations 6 --top_K 3 5 10 15 20 25 30 50 75 108 > $slurm_output_cub_resnet_concept_mask
+python ./src/codebase/concept_completeness_concept_mask_main.py --model "MoIE" --arch "ResNet101" --dataset "cub" --iterations 6 --top_K 3 5 10 15 20 25 30 50 75 108 > $slurm_output_cub_resnet_concept_mask
 
-python ../codebase/concept_completeness_main.py --model "MoIE" --epochs 75 --arch "ResNet101" --dataset "cub" --iterations 6 --top_K 3 5 10 15 20 25 30 50 75 108 > $slurm_output_cub_resnet_concept_completeness
+python ./src/codebase/concept_completeness_main.py --model "MoIE" --epochs 75 --arch "ResNet101" --dataset "cub" --iterations 6 --top_K 3 5 10 15 20 25 30 50 75 108 > $slurm_output_cub_resnet_concept_completeness
